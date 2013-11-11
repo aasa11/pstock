f2b2ccb7d450c1960c4f4d3490f9012feae9217e
aasa <anferneextt@gmail.com>
2013-11-11 16:48:43 +0800

    2013-11-11
    Add daily data import.

---
+++ getdailydata.py
@@ -0,0 +1,73 @@
+#!/usr/bin/
+#coding=gbk
+'''
+Created on 2013Äê11ÔÂ8ÈÕ
+
+@summary: 
+
+@author: huxiufeng
+'''
+import urllib
+import os
+import threading
+
+url_base = r'http://table.finance.yahoo.com/table.csv?s='
+
+def get_day_onestock(stockname, filename):
+    url = url_base + stockname
+    if stockname[0] == '6':
+        url += '.ss'
+    else : 
+        url += '.sz'
+        
+    urllink = urllib.urlopen(url)
+    filename = os.path.abspath(filename)
+    f = open(filename, 'w')
+    f.write(urllink.read())
+    f.close()
+
+
+def thread_day_stocks(stockids, respath, index,threadcount):
+    for i in range(index, len(stockids), threadcount):
+        stockname = stockids[i]
+        filename = os.path.join(os.path.abspath(respath), stockname+'.csv')
+        print "get day datas : ", stockname, " , ",filename
+        get_day_onestock(stockname, filename)
+        
+
+def get_day_stocks(cnffile, respath, threadcount = 1):
+    rf = open(os.path.abspath(cnffile), 'r')
+    stockids = []
+    for line in rf.readlines():
+        stockids.append(line[0:6])
+    
+    thdlist = []
+        
+    for index in range(threadcount):
+        threadhandle = threading.Thread(target=thread_day_stocks, args=(stockids, respath, index, threadcount))
+        threadhandle.start()
+        thdlist.append(threadhandle)
+        
+    for handle in thdlist:
+        handle.join()
+    
+        
+
+
+
+#----------------------It is a split line--------------------------------------
+
+def main():
+#     stockname = '600000'
+#     filename = 'res/600000.csv'
+#     get_day_onestock(stockname, filename)
+    cnffile = r'cnf/corp_codes.csv'
+    respath = r'res'
+    get_day_stocks(cnffile, respath, 20)
+    
+    
+#----------------------It is a split line--------------------------------------
+
+if __name__ == "__main__":
+    main()
+    print "It's ok"
---
+++ st_one.py
@@ -0,0 +1,92 @@
+#!/usr/bin/
+#coding=gbk
+'''
+Created on 2013Äê11ÔÂ11ÈÕ
+
+@summary: 
+
+@author: huxiufeng
+'''
+
+import os
+import time
+
+class st_para:
+    def __init__(self, name=None, date=None, v_open=None, 
+                 v_close=None, v_high=None, v_low=None, 
+                 volum=None, v_adjclose=None, index = 0):
+        self.name = name
+        self.date = date
+        self.v_open = v_open
+        self.v_cloes = v_close
+        self.v_high = v_high
+        self.v_low = v_low
+        self.volum = volum
+        self.v_adjclose = v_adjclose
+        self.index = index
+        
+    def get_data(self, datas,index = 0):
+        datalst = datas.split(',')
+        
+        if len(datalst) < 7:
+            return False
+        self.date =  time.strptime(datalst[0], "%Y-%m-%d")
+        self.v_open = float(datalst[1])
+        self.v_high = float(datalst[2])
+        self.v_low = float(datalst[3])
+        self.v_close = float(datalst[4])
+        self.volum = int(datalst[5])
+        self.v_adjclose = float(datalst[6])
+        self.index = index
+        
+        if self.volum <= 0:
+            return False
+        return True
+
+         
+
+
+class st_one:
+    def __init__(self, filename):
+        self.filename = os.path.abspath(filename)
+        
+    def imp_data(self):
+        self.datalst = []
+        f = open(self.filename, 'r')
+        for line in f.readlines():
+            if line.startswith('Date'):
+                continue
+            onedata = st_para()
+            #print line
+            rt = onedata.get_data(line)
+            if not rt:
+                self.datalst.append(onedata)
+                #print onedata.date
+        
+        self.datalst.sort(key=lambda x: x.date)
+        
+
+    def draw_dayline(self):
+        import matplotlib.pyplot as plt
+        plt.figure(1)
+        plt.plot(range(len(self.datalst)), [dt.v_close for dt in self.datalst])
+        plt.show()
+        
+        
+    
+            
+             
+
+#----------------------It is a split line--------------------------------------
+
+def main():
+    filename = 'res/600000.csv'
+    st = st_one(filename)
+    st.imp_data()
+    st.draw_dayline()
+    
+#----------------------It is a split line--------------------------------------
+
+if __name__ == "__main__":
+    main()
+    print "It's ok"
